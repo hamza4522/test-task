21117e8b02f9b55e88387fabb48cb5a4f75c451c
Hamza Ali <58938675+hamza4522@users.noreply.github.com>
2025-08-02 18:56:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from GitHub Actions CI/CD 🚀');
+  res.send('Hello from GitHub Actions CI/CD phase 2🚀');
 });
 
 app.listen(3000, () => {
